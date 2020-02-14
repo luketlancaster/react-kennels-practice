7323bd84be7151448a599fddb03df57f7d70ef2d
Luke Lancaster <luketlancaster@gmail.com>
2020-02-14 10:20:30 -0600

    Basic components setup and whatnot

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react"
+import './Employee.css';
+
+export default () => (
+  <section className="employee">
+    <h3 className="employee__name">Coolio McDoolio</h3>
+    <div className="employee__location">Nashville Kennels North</div>
+  </section>
+)
---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react"
+import './Location.css';
+
+export default () => (
+  <section className="location">
+    <h3 className="location__name">Nashville Kennels North</h3>
+    <div className="location__address">1234 N Bigtime Way</div>
+  </section>
+)
