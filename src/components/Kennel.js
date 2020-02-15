2e93467daabbf78a5734420c859e5236eacc4628
Luke Lancaster <luketlancaster@gmail.com>
2020-02-15 12:59:51 -0600

    Animal data provider/refactor

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import AnimalList from "./animal/AnimalList"
 import LocationList from "./location/LocationList"
 import { LocationProvider } from "./location/LocationProvider"
 import "./Kennel.css"
@@ -7,6 +9,11 @@ export default () => (
     <>
         <h2>Nashville Kennels</h2>
         <small>Loving care when you're not there.</small>
+
+        <h2>Animals</h2>
+        <AnimalProvider>
+            <AnimalList />
+        </AnimalProvider>
 
         <h2>Locations</h2>
         <LocationProvider>
---
+++ src/components/animal/AnimalProvider.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+export const AnimalContext = React.createContext();
+
+export const AnimalProvider = (props) => {
+  const [animals, setAnimals] = useState([]);
+
+  const getAnimals = () => {
+    return fetch('http://localhost:8088/animals')
+      .then(res => res.json())
+      .then(setAnimals)
+  }
+
+  const addAnimal = animal => {
+    return fetch('http://localhost:8088/animals', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(animal)
+    })
+      .then(getAnimals)
+  }
+
+  useEffect(() => {
+    getAnimals()
+  }, [])
+
+  useEffect(() => {
+    console.log('**** ANIMAL APPLICATION STATE CHANGED ****')
+  }, [animals])
+
+  return (
+    <AnimalContext.Provider value={{
+      animals, addAnimal
+    }}>
+      {props.children}
+    </AnimalContext.Provider>
+  )
+}
